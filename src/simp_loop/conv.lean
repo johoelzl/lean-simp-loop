198c8640136df4a5dd11007262e946f438ef2468
Johannes Hölzl <johannes.hoelzl@gmx.de>
2018-03-26 18:26:41 +0200

    WIP: continue

--- src/simp_loop/conv.lean
+++ src/simp_loop/conv.lean
@@ -28,6 +28,17 @@ namespace simp_loop
 meta structure conv_result (α : Type) :=
 (val : α) (rhs : expr) (proof : option expr)
 
+meta def conv_result.mmap_pr {α} (f_r : expr → tactic expr) (f_pr : expr → expr → tactic expr) :
+  conv_result α → tactic (conv_result α)
+| ⟨a, r, pr⟩ := do
+  r' ← f_r r,
+  match pr with
+  | some pr := do
+    pr' ← f_pr r pr,
+    return ⟨a, r', pr'⟩
+  | none := return ⟨a, r', none⟩
+  end
+
 meta structure conv_t (m : Type → Type) (α : Type) : Type :=
 (run : name → expr → m (conv_result α))
 
@@ -38,22 +49,13 @@ attribute [pp_using_anonymous_constructor] conv_t
 namespace conv_t
 
 section
-parameters {m : Type → Type} [monad m] [monad_tactic m]
+parameters {m : Type → Type} [monad m] [alternative m] [monad_tactic m]
 
-meta def lhs : conv_t m expr :=
-⟨λ r e, return ⟨e, e, none⟩⟩
+meta instance : monad_tactic (conv_t m) :=
+⟨λα t, ⟨λ r e, do a ← lift_tactic t, return ⟨a, e, none⟩⟩⟩
 
-meta def rel : conv_t m name :=
-⟨λ r e, return ⟨r, e, none⟩⟩
-
-meta def change (new_p : pexpr) : conv_t m unit :=
-⟨λ r e, do
-  new_e ← lift_tactic (do
-    e_type ← infer_type e,
-    new_e ← to_expr ``(%%new_p : %%e_type),
-    unify e new_e,
-    return new_e),
-  return ⟨(), new_e, none⟩⟩
+meta instance {α} : has_coe (tactic α) (conv_t m α) :=
+⟨lift_tactic⟩
 
 protected meta def pure {α : Type} : α → conv_t m α :=
 λ a, ⟨λr e, return ⟨a, e, none⟩⟩
@@ -82,174 +84,175 @@ protected meta def map {α β : Type} (f : α → β) (c : conv_t m α) : conv_t
   ⟨a, e₁, pr⟩ ← c.run r e,
   return ⟨f a, e₁, pr⟩⟩
 
-protected meta def bind {α β : Type} (c₁ : conv_t m α) (c₂ : α → conv_t m β) : conv_t m β :=
-⟨λ r e,
-  has_bind.bind (c₁.run r e) (λ⟨a, e₁, pr₁⟩,
-  has_bind.bind ((c₂ a).run r e₁) (λ⟨b, e₂, pr₂⟩,
-  has_bind.bind (lift_tactic $ join_proofs r pr₁ pr₂) (λpr, return ⟨b, e₂, pr⟩)))⟩
-/- do -- fails as it chooses the `conv.bind` as `bind`...
+-- do syntax doesn't work if we call it bind
+protected meta def bind' {α β : Type} (c₁ : conv_t m α) (c₂ : α → conv_t m β) : conv_t m β :=
+⟨λ r e, do
   ⟨a, e₁, pr₁⟩ ← c₁.run r e,
   ⟨b, e₂, pr₂⟩ ← (c₂ a).run r e₁,
   pr           ← lift_tactic $ join_proofs r pr₁ pr₂,
-  return ⟨b, e₂, pr⟩
-  -/
-
-meta instance : monad_tactic (conv_t m) :=
-⟨λα t, ⟨λ r e, do a ← (lift_tactic t : m α), return ⟨a, e, none⟩⟩⟩
+  return ⟨b, e₂, pr⟩⟩
 
 meta instance : monad (conv_t m) :=
-{ map  := @conv_t.map m _ _,
-  pure := @conv_t.pure m _ _,
-  bind := @conv_t.bind m _ _ }
+{ map  := @conv_t.map m _ _ _,
+  pure := @conv_t.pure m _ _ _,
+  bind := @conv_t.bind' m _ _ _ }
 
-/-
-protected meta def fail {α β : Type} [has_to_format β] (msg : β) : conv α :=
-⟨λ r e, tactic.fail msg⟩
+protected meta def fail {α β : Type} [has_to_format β] (msg : β) : conv_t m α :=
+tactic.fail msg
 
-protected meta def failed {α : Type} : conv α :=
-⟨λ r e, tactic.failed⟩
-protected meta def orelse {α : Type} (c₁ : conv α) (c₂ : conv α) : conv α :=
-λ r e, c₁ r e <|> c₂ r e
+protected meta def failed {α : Type} : conv_t m α :=
+tactic.failed
 
-meta instance : alternative conv :=
-{ failure := @conv.failed,
-  orelse  := @conv.orelse,
-  ..conv.monad }
--/
+protected meta def orelse {α : Type} (c₁ : conv_t m α) (c₂ : conv_t m α) : conv_t m α :=
+⟨λ r e, c₁.run r e <|> c₂.run r e⟩
 
-meta def whnf (md : transparency := reducible) : conv_t m unit :=
-⟨λ r e, do n ← (lift_tactic (tactic.whnf e md) : m expr), return ⟨(), n, none⟩⟩
+meta instance : alternative (conv_t m) :=
+{ failure := @conv_t.failed _ _ _ _,
+  orelse  := @conv_t.orelse _ _ _ _ }
 
-meta def dsimp : conv unit :=
-λ r e, do s ← simp_lemmas.mk_default, n ← s.dsimplify [] e, return ⟨(), n, none⟩
-
-meta def try {α} (c : conv α) : conv (option α) :=
+meta def try {α} (c : conv_t m α) : conv_t m (option α) :=
 (c >>= (return ∘ some)) <|> return none
 
-meta def tryb (c : conv unit) : conv bool :=
+meta def tryb (c : conv_t m unit) : conv_t m bool :=
 (c >> return tt) <|> return ff
 
-meta def trace {α : Type} [has_to_tactic_format α] (a : α) : conv unit :=
-λ r e, tactic.trace a >> return ⟨(), e, none⟩
+meta def lhs : conv_t m expr :=
+⟨λ r e, return ⟨e, e, none⟩⟩
 
-meta def trace_lhs : conv unit :=
+meta def rel : conv_t m name :=
+⟨λ r e, return ⟨r, e, none⟩⟩
+
+meta def guard_rel (r : name) : conv_t m unit :=
+do r' ← rel, guard $ r = r'
+
+meta def run' {α} (r : name) (e : expr) (c : conv_t m α) : conv_t m (conv_result α) :=
+⟨λr' e', do r ← c.run r e, return ⟨r, e', none⟩⟩
+
+meta def change_with (f : expr → tactic expr) : conv_t m unit :=
+⟨λ r e, do n ← lift_tactic $ f e, return ⟨(), n, none⟩⟩
+
+meta def change (new_p : pexpr) : conv_t m unit :=
+change_with $ λe, do
+  e_type ← infer_type e,
+  new_e ← to_expr ``(%%new_p : %%e_type),
+  unify e new_e,
+  return new_e
+
+meta def whnf (md : transparency := reducible) : conv_t m unit :=
+change_with $ λe, tactic.whnf e md
+
+meta def dsimp : conv_t m unit :=
+change_with $ λe, do s ← simp_lemmas.mk_default, s.dsimplify [] e
+
+meta def trace {α : Type} [has_to_tactic_format α] (a : α) : conv_t m unit :=
+tactic.trace a
+
+meta def trace_lhs : conv_t m unit :=
 lhs >>= trace
 
-meta def apply_lemmas_core (s : simp_lemmas) (prove : tactic unit) : conv unit :=
-λ r e, do
-  (new_e, pr) ← s.rewrite e prove r,
-  return ⟨(), new_e, some pr⟩
+meta def apply_lemmas_core (s : simp_lemmas) (prove : tactic unit) : conv_t m unit :=
+⟨λ r e, do
+  (new_e, pr) ← lift_tactic $ s.rewrite e prove r,
+  return ⟨(), new_e, some pr⟩⟩
 
-meta def apply_lemmas (s : simp_lemmas) : conv unit :=
-apply_lemmas_core s failed
+meta def apply_lemmas (s : simp_lemmas) : conv_t m unit :=
+apply_lemmas_core s tactic.failed
+
+meta def propext {α} (c : conv_t m α) : conv_t m α :=
+⟨λr lhs, do
+  guard (r = `eq),
+  r ← c.run `iff lhs,
+  lift_tactic $ r.mmap_pr return $ λrhs pr, mk_app `propext [pr]⟩
 
 /- adapter for using iff-lemmas as eq-lemmas -/
-meta def apply_propext_lemmas_core (s : simp_lemmas) (prove : tactic unit) : conv unit :=
-λ r e, do
-  guard (r = `eq),
-  (new_e, pr) ← s.rewrite e prove `iff,
-  new_pr ← mk_app `propext [pr],
-  return ⟨(), new_e, some new_pr⟩
+meta def apply_propext_lemmas_core (s : simp_lemmas) (prove : tactic unit) : conv_t m unit :=
+propext (apply_lemmas_core s prove)
 
-meta def apply_propext_lemmas (s : simp_lemmas) : conv unit :=
-apply_propext_lemmas_core s failed
+meta def apply_propext_lemmas (s : simp_lemmas) : conv_t m unit :=
+apply_propext_lemmas_core s tactic.failed
 
 private meta def mk_refl_proof (r : name) (e : expr) : tactic expr :=
 do env ← get_env,
    match (environment.refl_for env r) with
    | (some refl) := do pr ← mk_app refl [e], return pr
-   | none        := fail format!"converter failed, relation '{r}' is not reflexive"
+   | none        := tactic.fail format!"converter failed, relation '{r}' is not reflexive"
    end
 
 meta def to_tactic (c : conv unit) : name → expr → tactic (expr × expr) :=
 λ r e, do
-  ⟨u, e₁, o⟩ ← c r e,
+  ⟨u, e₁, o⟩ ← c.run r e,
   match o with
   | none   := do p ← mk_refl_proof r e, return (e₁, p)
   | some p := return (e₁, p)
   end
 
-meta def lift_tactic {α : Type} (t : tactic α) : conv α :=
-λ r e, do a ← t, return ⟨a, e, none⟩
+meta def apply_simp_set (attr_name : name) : conv_t m unit :=
+get_user_simp_lemmas attr_name >>= apply_lemmas
 
-meta def apply_simp_set (attr_name : name) : conv unit :=
-lift_tactic (get_user_simp_lemmas attr_name) >>= apply_lemmas
+meta def apply_propext_simp_set (attr_name : name) : conv_t m unit :=
+get_user_simp_lemmas attr_name >>= apply_propext_lemmas
 
-meta def apply_propext_simp_set (attr_name : name) : conv unit :=
-lift_tactic (get_user_simp_lemmas attr_name) >>= apply_propext_lemmas
-
-meta def skip : conv unit :=
+meta def skip : conv_t m unit :=
 return ()
 
-meta def repeat : conv unit → conv unit
-| c r lhs :=
-  (do
-     ⟨_, rhs₁, pr₁⟩ ← c r lhs,
-     guard (¬ lhs =ₐ rhs₁),
-     ⟨_, rhs₂, pr₂⟩ ← repeat c r rhs₁,
-     pr ← join_proofs r pr₁ pr₂,
-     return ⟨(), rhs₂, pr⟩)
-  <|> return ⟨(), lhs, none⟩
+meta def repeat (c : conv_t m unit) : conv_t m unit := do
+  old ← lhs,
+  c,
+  new ← lhs,
+  (if old =ₐ new then skip else repeat)
 
-meta def first {α : Type} : list (conv α) → conv α
-| []      := conv.failed
+meta def first {α : Type} : list (conv_t m α) → conv_t m α
+| []      := failed
 | (c::cs) := c <|> first cs
 
-meta def match_pattern (p : pattern) : conv (list level × list expr) :=
-do l ← lhs, lift_tactic $ tactic.match_pattern p l
+meta def match_pattern (p : pattern) : conv_t m (list level × list expr) :=
+do l ← lhs, tactic.match_pattern p l
 
-meta def mk_match_expr (p : pexpr) : tactic (conv unit) :=
-do new_p ← pexpr_to_pattern p,
-   return (λ r e, tactic.match_pattern new_p e >> return ⟨(), e, none⟩)
+meta def mk_match_expr (p : pexpr) : tactic (conv_t m unit) :=
+(λe, match_pattern e >> skip) <$> pexpr_to_pattern p
 
-meta def match_expr (p : pexpr) : conv unit :=
-λ r e, do
+meta def match_expr (p : pexpr) : conv_t m unit := do
   new_p ← pexpr_to_pattern p,
-  tactic.match_pattern new_p e >> return ⟨(), e, none⟩
+  match_pattern new_p,
+  skip
 
-meta def funext {α} (c : expr → conv α) : conv α :=
-λ r lhs, do
+meta def funext {α} (c : expr → conv_t m α) : conv_t m α :=
+⟨λr lhs, do
   guard (r = `eq),
-  (expr.lam n bi d b) ← return lhs,
-  let aux_type := expr.pi n bi d (expr.const `true []),
-  (result, _) ← solve_aux aux_type $ do {
-    x ← intro1,
-    c_result ← c x r (b.instantiate_var x),
-    let rhs  := expr.lam n bi d (c_result.rhs.abstract x),
-    match c_result.proof : _ → tactic (conv_result α) with
-    | some pr := do
-      let aux_pr := expr.lam n bi d (pr.abstract x),
-      new_pr ← mk_app `funext [lhs, rhs, aux_pr],
-      return ⟨c_result.val, rhs, some new_pr⟩
-    | none    := return ⟨c_result.val, rhs, none⟩
-    end },
-  return result
+  (expr.lam n bi d b) ← lift_tactic $ tactic.whnf lhs | lift_tactic $ failure,
+  n' ← lift_tactic $ mk_fresh_name,
+  let v := expr.local_const n' n bi d,
+  r ← (c v).run r lhs,
+  lift_tactic $ r.mmap_pr
+    (λr, return $ expr.lam n bi d (r.abstract v))
+    (λr pr, lift_tactic $ mk_app
+      `funext [lhs, expr.lam n bi d (r.abstract v), expr.lam n bi d (pr.abstract v)])⟩
 
-meta def congr_core {α β} (c_f : conv α) (c_a : conv β) : conv (option α × option β) :=
-λ r lhs, do
-  guard (r = `eq),
-  (expr.app f a) ← return lhs,
-  f_type ← infer_type f >>= tactic.whnf,
+meta def congr_core {α β} (c_f : conv_t m α) (c_a : conv_t m β) : conv_t m (α × β) := do
+  guard_rel `eq,
+  (expr.app f a) ← lhs | failure,
+  f_type ← lift_tactic (infer_type f >>= tactic.whnf),
   guard (f_type.is_arrow),
-  ⟨val_f, new_f, of⟩ ← try c_f r f,
-  ⟨val_a, new_a, oa⟩ ← try c_a r a,
-  rhs ← return $ new_f new_a,
-  match of, oa with
-  | none, none      :=
-      return ⟨(val_f, val_a), rhs, none⟩
-  | none, some pr_a := do
-      pr ← mk_app `congr_arg [a, new_a, f, pr_a],
-      return ⟨(val_f, val_a), new_f new_a, some pr⟩
-  | some pr_f, none := do
-      pr ← mk_app `congr_fun [f, new_f, pr_f, a],
-      return ⟨(val_f, val_a), rhs, some pr⟩
-  | some pr_f, some pr_a := do
-      pr ← mk_app `congr [f, new_f, a, new_a, pr_f, pr_a],
-      return ⟨(val_f, val_a), rhs, some pr⟩
-  end
+  ⟨λ r lhs, do
+    ⟨val_f, new_f, of⟩ ← c_f.run r f,
+    ⟨val_a, new_a, oa⟩ ← c_a.run r a,
+    let rhs := new_f new_a,
+    match of, oa with
+    | none, none      :=
+        return ⟨(val_f, val_a), rhs, none⟩
+    | none, some pr_a := do
+        pr ← lift_tactic $ mk_app `congr_arg [a, new_a, f, pr_a],
+        return ⟨(val_f, val_a), rhs, some pr⟩
+    | some pr_f, none := do
+        pr ← lift_tactic $ mk_app `congr_fun [f, new_f, pr_f, a],
+        return ⟨(val_f, val_a), rhs, some pr⟩
+    | some pr_f, some pr_a := do
+        pr ← lift_tactic $ mk_app `congr [f, new_f, a, new_a, pr_f, pr_a],
+        return ⟨(val_f, val_a), rhs, some pr⟩
+    end⟩
 
-meta def congr {α} (c : conv α) : conv (option α × option α) :=
-congr_core c c
+meta def congr (c : conv_t m unit) : conv_t m unit :=
+congr_core c c >> skip
 
 meta def bottom_up (c : conv unit) : conv unit :=
 λ r e, do
@@ -307,9 +310,13 @@ meta def findp : pexpr → conv unit → conv unit :=
   pat ← pexpr_to_pattern p,
   find_pattern pat c r e
 
+end
+
+end conv_t
+
 meta def conversion (c : conv unit) : tactic unit :=
 do (r, lhs, rhs) ← (target_lhs_rhs <|> fail "conversion failed, target is not of the form 'lhs R rhs'"),
-   (new_lhs, pr) ← to_tactic c r lhs,
+   (new_lhs, pr) ← conv_t.to_tactic c r lhs,
    (unify new_lhs rhs <|>
      do new_lhs_fmt ← pp new_lhs,
         rhs_fmt     ← pp rhs,
@@ -317,7 +324,5 @@ do (r, lhs, rhs) ← (target_lhs_rhs <|> fail "conversion failed, target is not 
                      rhs_fmt.indent 4 ++ format.line ++ "provided" ++
                      new_lhs_fmt.indent 4)),
    exact pr
-
-end conv
 
 end simp_loop
